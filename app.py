8816522eb9a2b36c3df93b02e3689d0d641626c6
tomohiko-urai <84952494+tomohiko-urai@users.noreply.github.com>
2022-04-14 19:14:41 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # 以下を「app.py」に書き込み
 import streamlit as st
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from PIL import Image
-from model import predict
+from model-1 import predict
 
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
@@ -34,13 +34,13 @@ if img_file is not None:
             st.write(str(round(result[2]*100, 2)) + "%の確率で" + result[0] + "です。")
 
         # 円グラフの表示
-        pie_labels = [result[1] for result in results[:n_top]]
-        pie_labels.append("others")
-        pie_probs = [result[2] for result in results[:n_top]]
-        pie_probs.append(sum([result[2] for result in results[n_top:]]))
-        fig, ax = plt.subplots()
-        wedgeprops={"width":0.3, "edgecolor":"white"}
-        textprops = {"fontsize":6}
-        ax.pie(pie_probs, labels=pie_labels, counterclock=False, startangle=90,
-               textprops=textprops, autopct="%.2f", wedgeprops=wedgeprops)  # 円グラフ
-        st.pyplot(fig)
+        #pie_labels = [result[1] for result in results[:n_top]]
+        #pie_labels.append("others")
+        #pie_probs = [result[2] for result in results[:n_top]]
+        #pie_probs.append(sum([result[2] for result in results[n_top:]]))
+        #fig, ax = plt.subplots()
+        #wedgeprops={"width":0.3, "edgecolor":"white"}
+        #textprops = {"fontsize":6}
+        #ax.pie(pie_probs, labels=pie_labels, counterclock=False, startangle=90,
+        #       textprops=textprops, autopct="%.2f", wedgeprops=wedgeprops)  # 円グラフ
+        #st.pyplot(fig)
